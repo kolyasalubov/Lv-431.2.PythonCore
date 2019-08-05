ffa9847774e79bde2308c1bc764a2421a75663d3
ashehed <53475942+ashehed@users.noreply.github.com>
2019-08-05 11:40:54 +0300

    Adding Shehedin_HW1

---
+++ shehedin_homework1_0.py
@@ -0,0 +1,9 @@
+a = int(input("a = ")) 
+b = int(input("b = "))
+
+concatenation = (a + b)
+subtraction = (a - b)
+multiplication = (a * b)
+division = (a / b)
+
+print (concatenation, subtraction, multiplication, division)
---
+++ shehedin_homework1_1.py
@@ -0,0 +1,13 @@
+user = input("What is your name? ")
+print ("Hello, ", user + "!")
+
+user_age = input("How old are you, " + user + "? ")
+print ("Great!")
+
+user_city = input ("And where do you live? ")
+print ((user_city) + " is really a nice city for old people!")
+
+print ("Let me summarize, your name is " + (user) + " and your age is " + (user_age) + ". You are from " + (user_city) + ". It's very nice to meet you, " + (user) + "!")
+
+
+    
