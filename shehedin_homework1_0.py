58390e500c5c2e88d4be2486096f747cd2ea30a3
ashehed <53475942+ashehed@users.noreply.github.com>
2019-08-05 12:45:24 +0300

    Adding updated Shehedin_HW1

--- shehedin_homework1_0.py
+++ shehedin_homework1_0.py
@@ -1,9 +1,9 @@
 a = int(input("a = ")) 
 b = int(input("b = "))
 
-concatenation = (a + b)
-subtraction = (a - b)
-multiplication = (a * b)
-division = (a / b)
+concatenation = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
 
-print (concatenation, subtraction, multiplication, division)
+print(concatenation, subtraction, multiplication, division)
